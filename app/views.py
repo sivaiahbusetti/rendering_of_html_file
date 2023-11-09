a410f9200f83c9669053016dd6d32d633599e5c9
Sivakumar busetti <sivakumarbusetti@gmail.com>
2023-11-09 10:02:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def siva(request):
+    return render(request,'siva.html')
+def kumar(request):
+    return render(request,'kumar.html')
